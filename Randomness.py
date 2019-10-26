125ad703f1af8ea34a6125449327a7e6d1e9fe34
Audeck <55060124+Audeck@users.noreply.github.com>
2019-10-26 21:25:24 +0200

    Testing capabilities

--- Randomness.py
+++ Randomness.py
@@ -6,7 +6,7 @@ def randomness(length):
 	n = length
 
 	for number in range(0,n): # Iterate n (== length) amount of times
-		number = number # Pointless line
+		number = number # Pointless line test
 		randomNum = randint(0, 67) # Can be 67 different characters -> 0 to 66 to weigh the selection
 		if randomNum in range(0, 52): # Add a letter
 			final_string += choice(string.ascii_letters)
