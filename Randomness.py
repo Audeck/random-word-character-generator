33642dab18124813d6afa40b8bf32acef6f8a51b
Audeck <55060124+Audeck@users.noreply.github.com>
2019-10-26 21:23:47 +0200

    Pointless line added

--- Randomness.py
+++ Randomness.py
@@ -5,7 +5,8 @@ def randomness(length):
 	final_string = ""
 	n = length
 
-	for x in range(0,n): # Iterate n (== length) amount of times
+	for number in range(0,n): # Iterate n (== length) amount of times
+		number = number # Pointless line
 		randomNum = randint(0, 67) # Can be 67 different characters -> 0 to 66 to weigh the selection
 		if randomNum in range(0, 52): # Add a letter
 			final_string += choice(string.ascii_letters)
