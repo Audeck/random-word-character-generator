1ae87981864b8f6b83db54767c0659c42d76ffda
Audeck <55060124+Audeck@users.noreply.github.com>
2020-01-01 12:01:41 +0100

    Added spacing (test)

--- Randomness.py
+++ Randomness.py
@@ -8,10 +8,13 @@ def randomness(length):
 	for number in range(0,n): # Iterate n (== length) amount of times
 		number = number # Pointless line test
 		randomNum = randint(0, 67) # Can be 67 different characters -> 0 to 66 to weigh the selection
+
 		if randomNum in range(0, 52): # Add a letter
 			final_string += choice(string.ascii_letters)
+
 		elif randomNum in range(52, 62): # Or add a digit
 			final_string += str(randint(0, 9))
+			
 		else: # Or add a special character
 			final_string += choice(["*", "~", "%", "#", "$"])
 
