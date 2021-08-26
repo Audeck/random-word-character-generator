3bdffb98690f6e03534de6bd273f5f60e91aa197
audeck <55060124+audeck@users.noreply.github.com>
2021-08-26 12:30:06 +0200

    Update Randomness.py

--- Randomness.py
+++ Randomness.py
@@ -3,11 +3,11 @@ from random import choice
 
 def randomness(length):
 	final_string = ""
-	possible_character_string = ascii_letters + "0123456789*~%#$"
+	possible_characters_string = ascii_letters + "0123456789*~%#$"
 
 	for number in range(0, length):
 		number = number # A completely pointless line
-		final_string += choice(possible_character_string)
+		final_string += choice(possible_characters_string)
 
 	return final_string
 
